65eb219fd998d0ef51e0a0b558b2c3ccf67e82b2
Gabriel Fiore <gabriel_fiore@hotmail.com>
2025-08-18 09:22:54 -0300

    exercicio01_condicional

--- CSStart/Program.cs
+++ CSStart/Program.cs
@@ -6,7 +6,29 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1 = 0;
+            int n2 = 0;
+
+            Console.WriteLine("Digite o primeiro número:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            n2 = int.Parse(Console.ReadLine());
+
+            int n3 = n1 + n2;
+
+            Console.Write("A soma dos números é: " + n3 + ". Portanto, ");
+
+            if (n3 == 0) {
+                Console.WriteLine("n3 is zero");
+            }
+            else if (n3 < 0)
+            {
+                Console.WriteLine("n3 is negative");
+            }
+            else if (n3 > 0)
+            {
+                Console.WriteLine("n3 is positive");
+            }
         }
     }
 }
